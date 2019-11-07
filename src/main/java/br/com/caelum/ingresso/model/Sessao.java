81c2ba5eee5a4c4c2d09317292ed396b54909baa
Bruno <bruno@hotmail.com>
2019-11-07 20:23:45 -0300

    Adicionado preco em Sessao, sala e filme e criado teste unitario

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,7 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalTime;
 
 import javax.persistence.Entity;
@@ -23,6 +25,8 @@ public class Sessao {
 	@ManyToOne
 	private Filme filme;
 	
+	private BigDecimal preco = BigDecimal.ZERO;
+	
 	public Sessao() {}
 	
 	public Sessao(LocalTime horario, Sala sala, Filme filme) {
@@ -30,8 +34,18 @@ public class Sessao {
 		this.horario = horario;
 		this.sala = sala;
 		this.filme = filme;
+		this.preco = sala.getPreco().add(filme.getPreco());
 	}
 	
+	
+	public BigDecimal getPreco() {
+		return preco.setScale(2, RoundingMode.HALF_UP);
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
 	public int getId() {
 		return id;
 	}
